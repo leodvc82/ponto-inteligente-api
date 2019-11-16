4bbda4b13d25b1b18ebc48e446f72c0133117c28
Leonardo <Leonardo@192.168.25.3>
2019-11-16 13:20:45 -0300

    Add Services/Impl/Tests empresa, funcionario e lancamento

---
+++ src/main/java/com/leodvc82/pontointeligente/api/services/EmpresaService.java
@@ -0,0 +1,25 @@
+package com.leodvc82.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import com.leodvc82.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaService {
+	
+	/**
+	 * Retorna uma empresa dado um CNPJ.
+	 * 
+	 * @param cnpj
+	 * @return Optional<Empresa>
+	 */
+	Optional<Empresa> buscarPorCnpj(String cnpj);
+	
+	/**
+	 * Cadastra uma nova empresa na base de dados.
+	 * 
+	 * @param empresa
+	 * @return Empresa
+	 */
+	Empresa persistir(Empresa empresa);
+
+}
